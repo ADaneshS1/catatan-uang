15876e98d62f6b9bd9fca069a812e2741cdde55d
Ahmad Danesh Sudiono <ahmaddaneshsudiono1.ppqita@gmail.com>
2024-05-21 11:20:38 +0700

    menambah tombol delete

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,6 +33,19 @@ export default function Home() {
       alert("EROR")
     })
   }, [])
+
+  const handleDelete = (id) => {
+    fetch(`/api/delData?id=${id}`, {
+      method:"DELETE",
+    })
+    .then((res) => res.json())
+    .then(() => {
+      router.reload()
+    })
+    .catch((err) => {
+      alert("Ada Eror ", err.message)
+    })
+  }
   return (
    <>
       <h2>Aplikasi Keuangan</h2>
@@ -59,8 +72,7 @@ export default function Home() {
                   <button onClick={() => router.push(`/detail/${data.id}`)}>Detail</button>
                   {" "}
                   <button onClick={() => router.push(`/edit/${data.id}`)}>Edit</button>
-
-
+                  <button onClick={() =>handleDelete(data.id)}>Delete</button>
               </div> 
             )
           })}
